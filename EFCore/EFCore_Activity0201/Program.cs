a8bc212f891d9b93836d7596abeca431302806b8
mostofa kamal <mostofakamal1990@gmail.com>
2023-07-31 07:23:56 +0600

    Initialize EfCore

---
+++ EFCore/EFCore_Activity0201/Program.cs
@@ -0,0 +1,24 @@
+﻿using EFCore_DBLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace EFCore_Activity0201
+{
+    internal class Program
+    {
+        private static DbContextOptionsBuilder<ApplicationDbContext> _optionsBuilder;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    
+
+        static void BuildOptions()
+        {
+            _optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            //_optionsBuilder.UseSqlServer(configuration.GetConnectionString( "AdventureWorks"));
+        }
+    }
+}
---
+++ EFCore/EFCore_DBLibrary/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_DBLibrary
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+
+        //Add a default constructor if scaffolding is needed
+        public ApplicationDbContext() { }
+        //Add the complex constructor for allowing Dependency Injection
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //intentionally empty.
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true,
+                    reloadOnChange: true);
+                _configuration = builder.Build();
+
+
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog= InventoryManagerDb; Trusted_Connection = True");
+            }
+        }
+
+    }
+}
